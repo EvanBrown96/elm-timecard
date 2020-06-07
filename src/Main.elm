f244d588af7980d76cce198cb00d8411baef4d50
Evan Brown <evanbrown@ku.edu>
2020-06-07 17:32:47 -0500

    initial support for multiple timers

--- src/Main.elm
+++ src/Main.elm
@@ -6,6 +6,7 @@ import Browser
 import Time
 import Time.Extra as Time
 import Task
+import Timer
 
 main : Program () Model Msg
 main =
@@ -19,85 +20,45 @@ main =
 
 -- MODEL
 
-type alias Millis = Int
-
-updated_time : Millis -> Time.Posix -> Time.Posix -> Millis
-updated_time offset start cur_time =
-  offset + Time.diff Time.Millisecond Time.utc start cur_time
-
-type TimerState
-  = Stopped  Millis
-  | Starting Millis
-  | Running  Millis Time.Posix
-  | Stopping Millis Time.Posix
-
 type alias Model =
-  { state : TimerState
-  , now   : Time.Posix
-  }
+  { now    : Time.Posix
+  , timers : List Timer.Timer}
 
 init : () -> (Model, Cmd Msg)
 init flags =
-  ( Model (Stopped 0) (Time.millisToPosix 0), Cmd.none )
-
+  ( Model (Time.millisToPosix 0) [], Cmd.none )
 
 -- UPDATE
 
 type Msg
-  = Start
-  | Stop
-  | Reset
+  = TimerCommand Int Timer.Msg
   | Tick Time.Posix
 
-display_time : Model -> Millis
-display_time model =
-  case model.state of
-    Stopped interval ->
-      interval
-    Starting interval ->
-      interval
-    Running interval start ->
-      updated_time interval start model.now
-    Stopping interval start ->
-      updated_time interval start model.now
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-    Start ->
-      case model.state of
-        Stopped interval ->
-          ( Model (Starting interval) model.now
-          , Task.perform Tick Time.now
-          )
+    TimerCommand index timer_msg ->
+      let start = List.take index model.timers
+          end   = List.drop index model.timers
+      in
+        case end of
+          [] ->
+            (model, Cmd.none)
 
-        _ ->
-          ( model, Cmd.none )
-
-    Stop ->
-      case model.state of
-        Running interval start ->
-          ( Model (Stopping interval start) model.now
-          , Task.perform Tick Time.now
-          )
-
-        _ ->
-          ( model, Cmd.none )
-
-    Reset ->
-      ( Model (Stopped 0) model.now, Cmd.none )
+          x::xs ->
+            let updated = Timer.update_state timer_msg x
+            in
+              ( (Model model.now (start ++ Tuple.first updated :: xs))
+              , Cmd.map (TimerCommand index) (Tuple.second updated)
+              )
 
     Tick cur_time ->
-      case model.state of
-        Starting interval ->
-          ( Model (Running interval cur_time) cur_time, Cmd.none )
-
-        Stopping interval start ->
-          ( Model (Stopped (updated_time interval start cur_time)) cur_time, Cmd.none )
-
-        _ ->
-          ( Model model.state cur_time, Cmd.none )
-
+      let updated = List.map (Timer.time_set cur_time) model.timers
+      in
+        ( (Model cur_time (List.map Tuple.first updated))
+        , Cmd.batch (List.indexedMap (\i c -> Cmd.map (TimerCommand i) (Tuple.second c)) updated)
+        )
 
 -- SUBSCRIPTIONS
 
@@ -108,19 +69,19 @@ subscriptions model =
 
 -- VIEW
 
-get_timer : Model -> Html Msg
-get_timer model =
-  div []
-    [ text (String.fromInt (display_time model))
-    , button [ onClick Start ] [ text "Start" ]
-    , button [ onClick Stop ] [text "Stop" ]
-    , button [ onClick Reset ] [text "Reset" ]
-    ]
+-- get_timer : Model -> Html Msg
+-- get_timer model =
+--   div []
+--     [ text (String.fromInt (display_time model))
+--     , button [ onClick Start ] [ text "Start" ]
+--     , button [ onClick Stop ] [text "Stop" ]
+--     , button [ onClick Reset ] [text "Reset" ]
+--     ]
 
 get_html : Model -> Browser.Document Msg
 get_html model =
   { title = "Elm Timecard"
-  , body = [ get_timer model ]
+  , body = [] -- get_timer model ]
   }
 
 view : Model -> Browser.Document Msg
---
+++ src/Timer.elm
@@ -0,0 +1,82 @@
+module Timer exposing (..)
+
+
+import Time
+import Time.Extra as Time
+import Task
+
+-- MODEL
+
+type alias Millis = Int
+
+type Timer
+  = Stopped Millis
+  | Starting Millis
+  | Running  Millis Time.Posix
+  | Stopping Millis Time.Posix
+
+updated_time : Millis -> Time.Posix -> Time.Posix -> Millis
+updated_time offset start cur_time =
+  offset + Time.diff Time.Millisecond Time.utc start cur_time
+
+display_time : Time.Posix -> Timer -> Millis
+display_time cur_time timer =
+  case timer of
+    Stopped interval ->
+      interval
+    Starting interval ->
+      interval
+    Running interval start ->
+      updated_time interval start cur_time
+    Stopping interval start ->
+      updated_time interval start cur_time
+
+
+-- UPDATE
+
+type Msg
+  = Start
+  | Stop
+  | Reset
+  | ImmediateUpdate Time.Posix
+
+update_state : Msg -> Timer -> (Timer, Cmd Msg)
+update_state msg timer =
+  case msg of
+      Start ->
+        case timer of
+          Stopped interval ->
+            ( Starting interval
+            , Task.perform ImmediateUpdate Time.now
+            )
+
+          _ ->
+            ( timer, Cmd.none )
+
+      Stop ->
+        case timer of
+          Running interval start ->
+            ( Stopping interval start
+            , Task.perform ImmediateUpdate Time.now
+            )
+
+          _ ->
+            ( timer, Cmd.none )
+
+      Reset ->
+        ( Stopped 0, Cmd.none )
+
+      ImmediateUpdate cur_time ->
+        time_set cur_time timer
+
+time_set : Time.Posix -> Timer -> (Timer, Cmd Msg)
+time_set cur_time timer =
+    case timer of
+      Starting interval ->
+        (Running interval cur_time, Cmd.none )
+
+      Stopping interval start ->
+        ( Stopped (updated_time interval start cur_time), Cmd.none )
+
+      _ ->
+        ( timer, Cmd.none )
