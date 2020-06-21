75a4367f4d2b10441e659597897687c095865042
Evan Brown <evanbrown@ku.edu>
2020-06-20 22:11:57 -0500

    add exclusive timer group

--- src/TimerGroup.elm
+++ src/TimerGroup.elm
@@ -6,11 +6,11 @@ import Time
 
 
 type TimerGroup =
-  TimerGroup Timer (List Timer)
+  TimerGroup Bool Timer (List Timer)
 
-newTimerGroup : String -> TimerGroup
-newTimerGroup name =
-  TimerGroup (Timer.newTimer name) []
+newTimerGroup : Bool -> String -> TimerGroup
+newTimerGroup exclusive name =
+  TimerGroup exclusive (Timer.newTimer name) []
 
 type Msg =
     Reset
@@ -36,10 +36,10 @@ cmdHandler index msg =
 updateState : Msg -> TimerGroup -> (TimerGroup, Cmd Msg)
 updateState msg timer_group =
   case timer_group of
-    TimerGroup main children ->
+    TimerGroup exclusive main children ->
       case msg of
         Reset ->
-          ( TimerGroup (Timer.newTimer main.name) (List.map (.name >> Timer.newTimer) children)
+          ( TimerGroup exclusive (Timer.newTimer main.name) (List.map (.name >> Timer.newTimer) children)
           , Cmd.none
           )
 
@@ -47,26 +47,44 @@ updateState msg timer_group =
           let
             ( updated_main, _ ) = Timer.updateState Timer.Start main
           in
-            Timer.forwardToIndex cmdHandler children index Timer.Start
-            |> Tuple.mapFirst (TimerGroup updated_main)
+            case exclusive of
+              False ->
+                Timer.forwardToIndex cmdHandler children index Timer.Start
+                |> Tuple.mapFirst (TimerGroup exclusive updated_main)
+
+              True ->
+                let
+                  start = List.take index children
+                  end   = List.drop index children
+                in
+                  case end of
+                    x::xs ->
+                      List.map (Timer.updateState Timer.Stop) start
+                      ++ Timer.updateState Timer.Start x
+                      :: List.map (Timer.updateState Timer.Stop) xs
+                      |> List.unzip
+                      |> Tuple.mapBoth
+                           (TimerGroup exclusive updated_main)
+                           (\l -> List.indexedMap (\i -> Cmd.map (cmdHandler i)) l |> Cmd.batch )
+
+                    _ -> ( timer_group, Cmd.none )
 
         Stop index ->
           let
             ( updated_children, cmd ) = Timer.forwardToIndex cmdHandler children index Timer.Stop
-            all_stopped               = List.filter Timer.isRunning updated_children
-                                        |> List.isEmpty
+            all_stopped               = List.all (not << Timer.isRunning) updated_children
             ( updated_main, _ )       = if all_stopped
                                         then Timer.updateState Timer.Stop main
                                         else ( main, Cmd.none )
           in
-            ( TimerGroup updated_main updated_children, cmd )
+            ( TimerGroup exclusive updated_main updated_children, cmd )
 
         Update index cur_time ->
           let
             ( updated_main, _ ) = Timer.updateState (Timer.Update cur_time) main
           in
             Timer.forwardToIndex cmdHandler children index (Timer.Update cur_time)
-            |> Tuple.mapFirst (TimerGroup updated_main)
+            |> Tuple.mapFirst (TimerGroup exclusive updated_main)
 
         UpdateAll cur_time ->
           let
@@ -75,5 +93,5 @@ updateState msg timer_group =
             List.map (Timer.updateState (Timer.Update cur_time)) children
             |> List.unzip
             |> Tuple.mapBoth
-                 (TimerGroup updated_main)
+                 (TimerGroup exclusive updated_main)
                  (\l -> List.indexedMap (\i -> Cmd.map (cmdHandler i)) l |> Cmd.batch )
